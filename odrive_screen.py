e4e2cd326b702595c271ace551dfb0beef35b1b7
softdev <herkyaaron@gmail.com>
2025-10-17 11:54:35 -0700

    pos printer

--- odrive_screen.py
+++ odrive_screen.py
@@ -7,11 +7,16 @@ ODRIVE_CONTROLLER_SERIAL_NUMBER = "207935A1524B"
 od = find_odrive(serial_number=ODRIVE_CONTROLLER_SERIAL_NUMBER)
 ax = ODriveAxis(od.axis1)
 ax.set_vel_limit(5) #turns/s
+# ax.home_without_endstop(1, 0.5)
 
+#4 butt 3 pot
 
 
 class OdriveScreen(Screen):
     button_shift = NumericProperty(0)
+
+    BUTTON_PIN = 4
+    POT_PIN = 3
     debounce = False
     def __init__(self, **kwargs):
         super(Screen, self).__init__(**kwargs)
@@ -41,14 +46,22 @@ class OdriveScreen(Screen):
         ax.set_relative_pos(-5)
 
     def waitForNextCommand(self, time=None):
+        print(round(ax.get_pos(), 2))
         if not ax.is_busy():
            self.debounce = False
+
+    def btnPress(self):
+        return not digital_read(self.BUTTON_PIN)
+
+
+
 
     def home(self):
         if self.debounce:
             return
         print("homing")
         self.debounce = True
-        ax.home_with_endstop(1, .5, 2)
+        # ax.home_with_endstop(1, .5, 2)
+        ax.home_without_endstop(1, .5)
 
 
